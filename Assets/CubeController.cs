141c17883ca1f6bab2a4121af7a64fd1d5036ec3
yoshiteru.natsu <yoshiteru.natsu@outlook.com>
2018-01-24 12:25:10 +0900

    2D課題

---
+++ Assets/CubeController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour
+{
+
+
+    // キューブの移動速度
+    private float speed = -0.2f;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Cube" || other.gameObject.tag == "Ground")
+        {
+            this.GetComponent<AudioSource>().Play();
+            Debug.Log("s");
+        }
+        else
+        {
+            this.GetComponent<AudioSource>().Stop();
+        }
+    }
+}
